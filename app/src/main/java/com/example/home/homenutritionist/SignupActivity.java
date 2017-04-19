185bfaff773d157428e56aa9f2694c00633f7a34
user <ykchung@cityu.edu.hk>
2017-04-20 03:31:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/home/homenutritionist/SignupActivity.java
@@ -0,0 +1,54 @@
+package com.example.home.homenutritionist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class SignupActivity extends AppCompatActivity {
+
+    Button myButton1;
+    Button myButton2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        // setSignupButton();
+        myButton1 = (Button) findViewById(R.id.btnSignup2);
+
+        myButton1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                EditText e1 = (EditText)findViewById(R.id.editText4);
+                EditText e2 = (EditText)findViewById(R.id.editText5);
+                EditText e3 = (EditText)findViewById(R.id.editText3);
+                if(e1.getText().toString().equals( e2.getText().toString())){
+                    startActivity(new Intent(SignupActivity.this, HomeActivity.class));
+                    Toast.makeText(SignupActivity.this,
+                            "Welcome " + e3.getText(), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SignupActivity.this,
+                            "The passwords are not the same.", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+        // setCancelButton();
+        myButton2 = (Button) findViewById(R.id.btnCancel);
+
+        myButton2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(SignupActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+
+}
